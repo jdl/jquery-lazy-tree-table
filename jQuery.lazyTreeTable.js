ff030290fb1e2406f6d2abfba9384ace3fa72570
James Ludlow <jdl@james-ludlows-computer.local>
2009-07-30 08:40:39 -0500

    Now supports either TR (default) or TBODY tags as the tree nodes.

--- jQuery.lazyTreeTable.js
+++ jQuery.lazyTreeTable.js
@@ -13,48 +13,55 @@
 */
 ;(function($) {
   $.fn.lazyTreeTable = function(options) {
-    debug(this);
     var defaults = {
       childFetchPath: "#",
-      childRowClassPrefix: "child-of-",
-      childPlaceholderRowClass: "child-placeholder",
+      childNodeClassPrefix: "child-of-",
+      childPlaceholderNodeClass: "child-placeholder",
       expandClass: "expand",
       collapseClass: "collapse",
       spinnerClass: "spinner",
-      childTypePrefix: "child-type-"  // Only needed if you want to have a parent with multiple sets of children.
+      childTypePrefix: "child-type-",  // Only needed if you want to have a parent with multiple sets of children.
+      parentHtmlType: "tr" // 'tr' or 'tbody'. Complex tables which use rowspan can't really use a tr as a parent. Each node should be grouped into it's own tbody.
+      
     };
     var opts = $.extend(defaults, options);
     
-    var childOfRegex = new RegExp(opts.childRowClassPrefix);
+    var childOfRegex = new RegExp(opts.childNodeClassPrefix);
     var hasChildTypeRegex = new RegExp(opts.childTypePrefix);
     
     return this.each(function() {
-      $('#' + this.id + '>tbody>tr').each(function(i, row) {
-        initRowLinks(row);
-        if(!isChild(row)) {
-          hideDescendents(row);
+      var parentSelector = '#' + this.id;
+      if (opts.parentHtmlType == 'tr') {
+        parentSelector = parentSelector + '>tbody>tr';
+      } else if (opts.parentHtmlType == 'tbody') {
+        parentSelector = parentSelector + '>tbody';
+      }
+      $(parentSelector).each(function(i, node) {
+        initNodeLinks(node);
+        if(!isChild(node)) {
+          hideDescendents(node);
         }
       });
     });
     
-    // For a row's expand/collapse links, perform the following:
+    // For a node's expand/collapse links, perform the following:
     // * Hide the collapse link(s)
     // * Show any expand links for which there are children of that type.
-    function initRowLinks(row) {
-      var collapseLinks = getCollapseLinks(row);
-      var expandLinks = getExpandLinks(row);
+    function initNodeLinks(node) {
+      var collapseLinks = getCollapseLinks(node);
+      var expandLinks = getExpandLinks(node);
       
       // Hide all of the collapse buttons.
       if (collapseLinks.length > 0) {
         $(collapseLinks).hide();
-        $(collapseLinks).bind("click", {element: row}, handleCollapseEvent);
+        $(collapseLinks).bind("click", {element: node}, handleCollapseEvent);
       }
       
       if (expandLinks.length > 0) {
         $(expandLinks).each(function(i, link) {
-          if (hasChildren(row, matchingClassFromElement(hasChildTypeRegex, link))) {
+          if (hasChildren(node, matchingClassFromElement(hasChildTypeRegex, link))) {
             $(link).show();
-            $(link).bind("click", {element: row}, handleExpandEvent);
+            $(link).bind("click", {element: node}, handleExpandEvent);
           } else {
             $(link).hide();
           }
@@ -62,155 +69,162 @@
       }
     };
     
-    // Applies the supplied function to all descendents of the parentRow, but not to 
-    // the parentRow itself.
-    function applyToBranch(parentRow, fn) {
-      getChildren(parentRow).each(function(i, child) {
+    // Applies the supplied function to all descendents of the parentNode.
+    // params
+    // * parentNode: The top-level node of the branch to which we should apply the function.
+    // * applyToParent: If true, then the parentNode itself will also have the function applied.
+    // * fn: The function to apply to each node in this branch of the tree.
+    function applyToBranch(parentNode, fn) {
+      getChildren(parentNode).each(function(i, child) {
         fn(child);
         applyToBranch(child, fn);
       });
     };
     
-    // Given a parent row, hide all descendents.
-    function hideDescendents(parentRow) {
-      applyToBranch(parentRow, function(row) {
-        $(row).hide();
+    // Given a parent node, hide all descendents.
+    function hideDescendents(parentNode) {
+      applyToBranch(parentNode, function(node) {
+        $(node).hide();
       });
     };
     
-    // Returns the parent row to the supplied row, or null.
-    function getParent(row) {
-      var childOfClassName = matchingClassFromElement(childOfRegex, row);
+    // Returns the parent node to the supplied node, or null.
+    function getParent(node) {
+      var childOfClassName = matchingClassFromElement(childOfRegex, node);
       if (childOfClassName) {
-        var parentId = childOfClassName.slice(opts.childRowClassPrefix.length);
+        var parentId = childOfClassName.slice(opts.childNodeClassPrefix.length);
         // The call to .parent() here is the jQuery call that will give us the 
-        // tbody (or table) that contains this row. This is being used to scope the
+        // tbody (or table) that contains this node. This is being used to scope the
         // find to only this table in case there is a duplicate ID in another table
         // somewhere else on the page.
-        var parent = $(row).parent().find('#' + parentId);
+        var parent = $(node).parent().find('#' + parentId);
         return parent;
       } else {
         return null;
       }
     };
     
-    // Checks to see if a given row ID is an ancestor of a given row.
-    function hasAncestor(row, ancestorId) {
-      var parentRow = getParent(row);
-      if (parentRow) {
-        return parentRow.attr('id') == ancestorId || hasAncestor(parentRow, ancestorId);
+    // Checks to see if a given node ID is an ancestor of a given node.
+    function hasAncestor(node, ancestorId) {
+      var parentNode = getParent(node);
+      if (parentNode) {
+        return parentNode.attr('id') == ancestorId || hasAncestor(parentNode, ancestorId);
       } else {
         return false;
       }
     };
     
-    // Given a node (row), this returns true if there is another row
+    // Given a node (node), this returns true if there is another node
     // which lists this node as a parent.
-    function hasChildren(row) {
-      if (row != null && getChildren(row).length > 0) {
+    function hasChildren(node) {
+      if (node != null && getChildren(node).length > 0) {
         return true;
       } else {
         return false;
       }
     };
     
-    // Returns an array of rows which are direct descendents of the passed-in row.
-    // If a childType is specifed, only child rows with that class will be considered.
-    function getChildren(row, childType) {
+    // Returns an array of nodes which are direct descendents of the passed-in node.
+    // If a childType is specifed, only child nodes with that class will be considered.
+    function getChildren(node, childType) {
+      // A node without an ID can't, by definition, have any children.
+      if (!node.id) {
+        return $([]);
+      }
+      
       // Don't let 'siblings' confuse you here.  That's in terms of the table, where all of the
-      // rows are siblings. The 'children' we're looking for are other rows which reference the
-      // current row as a parent.
-      var selectorString = '.' + opts.childRowClassPrefix + row.id;
+      // nodes are siblings. The 'children' we're looking for are other nodes which reference the
+      // current node as a parent.
+      var selectorString = '.' + opts.childNodeClassPrefix + node.id;
       if (childType) {
         selectorString = selectorString + "." + childType;
       }
-      return $(row).siblings(selectorString);
+      return $(node).siblings(selectorString);
     };
     
     // There should only be one placeholder child per parent, but this returns all of them if it
     // finds more than one.
-    function getPlaceholderChildren(row, childType) {
-      return getChildren(row, childType).filter('.' + opts.childPlaceholderRowClass)
+    function getPlaceholderChildren(node, childType) {
+      return getChildren(node, childType).filter('.' + opts.childPlaceholderNodeClass)
     }
     
-    // Given a node (row), this returns true if this row is a child of
-    // another row.
-    function isChild(row) {
-      if (row != null && $(row).attr('class').match(childOfRegex)) {
+    // Given a node (node), this returns true if this node is a child of
+    // another node.
+    function isChild(node) {
+      if (node != null && $(node).attr('class').match(childOfRegex)) {
         return true;
       } else {
         return false;
       }
     };
     
-    // Given a parent row, this will expand (show) all of its child rows.
+    // Given a parent node, this will expand (show) all of its child nodes.
     // This only drills down one level in the tree.  It won't open all of the 
     // descendents.
-    function expandChildren(parentRow, childType) {
-      if (getPlaceholderChildren(parentRow, childType).length > 0) {
-        lazyLoadChildren(parentRow, childType);
+    function expandChildren(parentNode, childType) {
+      if (getPlaceholderChildren(parentNode, childType).length > 0) {
+        lazyLoadChildren(parentNode, childType);
       }
-      getChildren(parentRow, childType).each(function(i, child) {
+      getChildren(parentNode, childType).each(function(i, child) {
         $(child).show();
       });
     };
     
-    // Given a parent row, this finds any children which are currently placeholders and replaces them
+    // Given a parent node, this finds any children which are currently placeholders and replaces them
     // with data from the server.
-    function lazyLoadChildren(parentRow, childType) {
-      $('td:first', parentRow).addClass(opts.spinnerClass);
+    function lazyLoadChildren(parentNode, childType) {
+      $('td:first', parentNode).addClass(opts.spinnerClass);
       $.get(opts.childFetchPath, {child_type: childType}, function(data) {
-        getPlaceholderChildren(parentRow, childType).remove();
-        $(parentRow).after(data);
+        getPlaceholderChildren(parentNode, childType).remove();
+        $(parentNode).after(data);
         
         // Hide any grandchildren (or lower) nodes from the parent that
         // was just expanded.
-        var children = getChildren(parentRow);
-        children.each(function(i, newRow){
-          applyToBranch(newRow, function(row) {
-            $(row).hide();
+        getChildren(parentNode).each(function(i, newNode){
+          applyToBranch(newNode, function(node) {
+            $(node).hide();
           });
         });
       
-        // Initialize the newly injected rows.
+        // Initialize the newly injected nodes.
         // First, set up the expand/collapse links for this branch.
-        applyToBranch(parentRow, function(row) {
-          initRowLinks(row);
+        applyToBranch(parentNode, function(node) {
+          initNodeLinks(node);
         });
         
-        $('td:first', parentRow).removeClass(opts.spinnerClass);
+        $('td:first', parentNode).removeClass(opts.spinnerClass);
       });
     };
     
     /* Someone clicked an "expand" link.
      * Several things are about to happen:
      *   1. Hide the clicked expand link.
-     *   2. If there is a child placeholder, fetch the child rows remotely.
-     *   3. Show the child rows.
+     *   2. If there is a child placeholder, fetch the child nodes remotely.
+     *   3. Show the child nodes.
      *   4. Show the collapse link. If the expand link is tied to a child type, then only show the 
      *      corresponding collapse link.
-     *   5. If non-placeholder children exist for a different child type, remove those rows and replace 
-     *      them with a placeholder child row.
+     *   5. If non-placeholder children exist for a different child type, remove those nodes and replace 
+     *      them with a placeholder child node.
      */
     function handleExpandEvent(event) {
-      var row = event.data.element;
+      var node = event.data.element;
       var childType = matchingClassFromElement(hasChildTypeRegex, event.target);
       
       // Hide the link that was clicked.
       $(event.target).hide();
-      expandChildren(row, childType);
-      $(getCollapseLinks(row, childType)).show();
-      replaceChildrenWithPlaceholder(row, childType);
+      expandChildren(node, childType);
+      $(getCollapseLinks(node, childType)).show();
+      replaceChildrenWithPlaceholder(node, childType);
       
       // If we're dealing with a child type, make sure that for the other types
       // only the expand links are showing.
       if (childType) {
-        $(getExpandLinks(row)).each(function(i, link){
+        $(getExpandLinks(node)).each(function(i, link){
           if(matchingClassFromElement(hasChildTypeRegex, link) != childType) {
             $(link).show();
           }
         });
-        $(getCollapseLinks(row)).each(function(i, link){
+        $(getCollapseLinks(node)).each(function(i, link){
           if(matchingClassFromElement(hasChildTypeRegex, link) != childType) {
             $(link).hide();
           }
@@ -225,11 +239,11 @@
      * Checks for any non-placeholder children which are not of the ignoredChildType
      * and replaces them with a single child placeholder per child type.
      */
-    function replaceChildrenWithPlaceholder(row, ignoredChildType) {
+    function replaceChildrenWithPlaceholder(node, ignoredChildType) {
       var childTypesToBeReplaced = [];
-      getChildren(row).each(function(i, child) {
-        if (!$(child).hasClass(opts.childPlaceholderRowClass)) {
-          // This child row is not already a placeholder.
+      getChildren(node).each(function(i, child) {
+        if (!$(child).hasClass(opts.childPlaceholderNodeClass)) {
+          // This child node is not already a placeholder.
           thisChildType = matchingClassFromElement(hasChildTypeRegex, child);
           if (thisChildType != '' && thisChildType != ignoredChildType) {
             // And it's not of the ignored child type.
@@ -241,50 +255,58 @@
         }
       });
       
-      for(var i = 0; i < childTypesToBeReplaced.length; i++) {
-        // Remove the children of this type.
-        getChildren(row, childTypesToBeReplaced[i]).remove();
+      for(var idx = 0; idx < childTypesToBeReplaced.length; idx++) {
+        // Remove the children of this type, and all of their descendents.
+        getChildren(node, childTypesToBeReplaced[idx]).each(function(i, childNode) {
+          applyToBranch(childNode, function(n) {
+            $(n).remove();
+          });
+          $(childNode).remove();
+        });
         // Inject a new child placeholder of the appropriate type.
-        var placeholderClass = opts.childRowClassPrefix + row.id + ' ' + opts.childPlaceholderRowClass + ' ' + childTypesToBeReplaced[i];
-        $(row).after('<tr class="' + placeholderClass + '"></tr>');
+        var placeholderClass = opts.childNodeClassPrefix + node.id + ' ' + opts.childPlaceholderNodeClass + ' ' + childTypesToBeReplaced[idx];
+        $(node).after('<tr class="' + placeholderClass + '"></tr>');
       }
     };
     
-    // Given a parent row, this will collapse (hide) all of its child rows.
-    function collapseChildren(parentRow) {
-      getChildren(parentRow).each(function(i, child) {
+    // Given a parent node, this will collapse (hide) all of its child nodes including grandchildren, etc.
+    // Any expand/collapse links on these descendents will also be reset.
+    function collapseChildren(parentNode) {
+      applyToBranch(parentNode, function(child) {
+        $(getCollapseLinks(child)).hide();
+        $(getExpandLinks(child)).show();
         $(child).hide();
       });
     };
     
     // Someone clicked a "collapse" link.
-    // Note that the element being passed into here is the row which contains the link.
+    // Note that the element being passed into here is the node which contains the link.
     function handleCollapseEvent(event) {
-      var row = event.data.element;
+      var node = event.data.element;
       var childType = matchingClassFromElement(hasChildTypeRegex, event.target);
       
-      $(getCollapseLinks(row, childType)).hide();
-      collapseChildren(row);
-      $(getExpandLinks(row, childType)).show();
+      $(getCollapseLinks(node, childType)).hide();
+      collapseChildren(node);
+      $(getExpandLinks(node, childType)).show();
       event.preventDefault();
     };
     
-    // Finds the "expand" links in a given row.
-    function getExpandLinks(row, childType) {
+    // Finds the "expand" links in a given node.
+    function getExpandLinks(node, childType) {
       var classString = '.' + opts.expandClass;
       if (childType) {
         classString = classString + '.' + childType;
       }
-      return $(classString, $(row), $('td:first > a'))
+      return $(classString, $(node), $('td:first > a'))
     };
 
-    // Finds the "collapse" links in a given row.
-    function getCollapseLinks(row, childType) {
+    // Finds the "collapse" links in a given node.
+    function getCollapseLinks(node, childType) {
       var classString = '.' + opts.collapseClass;
       if (childType) {
         classString = classString + '.' + childType;
       }
-      return $(classString, $(row), $('td:first > a'))
+      return $(classString, $(node), $('td:first > a'))
     };
 
     // Given an array of strings, return a single string with each array element
@@ -331,9 +353,5 @@
       return false;
     };
 
-    
-    function debug(obj) {
-      //console.debug("obj: " + obj.toString());
-    };
   };
 })(jQuery);
